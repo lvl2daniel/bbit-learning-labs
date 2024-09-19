ce4d8eddac79a0b44434a18f24edd54d049a0c0d
Daniel Gonzalez <Danielgonzalez4444@yahoo.com>
2024-09-19 19:53:48 -0400

    Sustain correct channel reference in consumer, remove misplaced connection closures

--- Tech-Lab-On-Campus/Producer-And-Consumer/consumer/solution/consumer_sol.py
+++ Tech-Lab-On-Campus/Producer-And-Consumer/consumer/solution/consumer_sol.py
@@ -19,35 +19,27 @@ class mqConsumer(mqConsumerInterface):
     # finally set up a callback function for receiving messages
     def setupRMQConnection(self) -> None:
         conParams = pika.URLParameters(os.environ['AMQP_URL'])
-        connection = pika.BlockingConnection(parameters=conParams)
-        channel = connection.channel()
-        channel.exchange_declare(exchange=self.exchange_name, exchange_type='direct')
-        channel.queue_declare(queue=self.queue_name, durable=True)
-        channel.queue_bind(exchange=self.exchange_name, routing_key=self.binding_key, queue=self.queue_name)
-        channel.basic_consume(queue=self.queue_name, on_message_callback=self.on_message_callback, auto_ack=True)
+        self.connection = pika.BlockingConnection(parameters=conParams)
+        self.channel = self.connection.channel()
+        self.channel.exchange_declare(exchange=self.exchange_name, exchange_type='direct')
+        self.channel.queue_declare(queue=self.queue_name, durable=True)
+        self.channel.queue_bind(exchange=self.exchange_name, routing_key=self.binding_key, queue=self.queue_name)
+        self.channel.basic_consume(queue=self.queue_name, on_message_callback=self.on_message_callback, auto_ack=False)
 
     # Print the UTF-8 string message and then close the connection.
     def on_message_callback(self, channel, method_frame, header_frame, body) -> None:
         # Acknowledge message
-        channel.basic_ack(delivery_tag=method_frame.delivery_tag)
+        self.channel.basic_ack(delivery_tag=method_frame.delivery_tag, multiple=False)
         print(f"{body.decode()}")
-        self.__del__()
 
     def __del__(self) -> None:
         print("Closing RMQ connection on destruction")
-        conParams = pika.URLParameters(os.environ['AMQP_URL'])
-        connection = pika.BlockingConnection(parameters=conParams)
-        channel = connection.channel()
-        channel.close()
-        connection.close()
+        self.channel.close()
+        self.connection.close()
 
     def startConsuming(self) -> None:
         print(" [*] Waiting for messages. To exit press CTRL+C")
-        conParams = pika.URLParameters(os.environ['AMQP_URL'])
-        connection = pika.BlockingConnection(parameters=conParams)
-        connection.process_data_events()
-        connection.close()
-        os._exit(0)
+        self.connection.process_data_events()
         
     
     
